b318ef814d887f98492a833dcca683360adb8cbf
Bhanuka <bhanukalm@gmail.com>
2020-09-01 11:24:35 +0530

    Timer app

---
+++ src/components/timer.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import TimeDisplay from './time-display';
+
+const TimerFrame = styled.div`
+  display: inline-block;
+  display: flex;
+  width: 500px;
+  flex-direction: column;
+
+  margin: 20px;
+  box-shadow: 0px 0px 11px 2px #cecece;
+  border-radius: 13px;
+  align-items: center;
+`;
+
+const Button = styled.button`
+  padding: 20px;
+  border: none;
+  margin: 20px;
+  border-radius: 13px;
+  color: #fff;
+  background: #536dfe;
+`;
+
+interface Props {
+  interval?: number;
+  name?: string;
+  updateTotalTime: Function;
+}
+
+export const Timer: React.FC<Props> = ({
+  interval = 10,
+  name = 'Timer',
+  updateTotalTime,
+}: Props) => {
+  const [intervalId, setIntervalId]: [any, Function] = useState(null);
+  const [time, setTime] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  const addTime = (add: any) => {
+    setTime((prevTime) => {
+      const newTime = prevTime + add;
+      updateTotalTime(newTime);
+      return newTime;
+    });
+  };
+
+  const play = () => {
+    setIntervalId(setInterval(() => addTime(interval), interval));
+    setIsPlaying(true);
+  };
+
+  const pause = () => {
+    clearInterval(intervalId);
+    setIsPlaying(false);
+  };
+
+  const reset = () => {
+    setTime(0);
+    updateTotalTime(0);
+  };
+
+  return (
+    <TimerFrame>
+      <h2>{name}</h2>
+      <TimeDisplay time={time} />
+      <div>
+        {isPlaying ? (
+          <Button onClick={() => pause()}>Pause</Button>
+        ) : (
+          <Button onClick={() => play()}>Play</Button>
+        )}
+        <Button onClick={() => reset()}>Reset</Button>
+      </div>
+    </TimerFrame>
+  );
+};
